65319aa182e69bbf0c2b287e1a44919b45ee559f
Ahmed Zribi <zribi.ahmed@perfectsoft.one>
2019-12-24 13:40:33 +0100

    set up the graphql server using the package 'graphql-yoga'

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
-console.log('Set up');
+import {GraphQLServer} from 'graphql-yoga';
+
+// Types definitions
+const typeDefs = `
+    type Query {
+        test: String!
+        fullName: String!
+    }
+`
+
+// Resolvers
+const resolvers = {
+    Query: {
+        test() {
+            return ('testing first query')
+        },
+        fullName() {
+            return 'Ahmed ZRIBI'
+        }
+    }
+}
+
+const server = new GraphQLServer({
+    typeDefs,
+    resolvers
+})
+
+server.start({port: 2000}, () => console.log('Server is running'));
