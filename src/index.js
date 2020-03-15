b5e8ae8a5f6acfc1d9eac347fff3b8a88fdf2599
zdahmed93 <ahmedzribiahmed@gmail.com>
2020-03-15 02:30:06 +0100

    move type definitions to a seperate .graphql file

--- src/index.js
+++ src/index.js
@@ -75,70 +75,6 @@ let comments = [
 // Scalar types : String / Int / Float / Boolean / ID
 
 
-// Types definitions
-const typeDefs = `
-    type Query {
-        me: User!
-        post: Post!
-        users(query: String): [User!]!
-        posts(query: String): [Post!]!
-        comments: [Comment!]!
-    }
-
-    type Mutation {
-        createUser(data: CreateUserInput!): User!
-        deleteUser(id: ID!): User!
-        createPost(data: CreatePostInput!): Post!
-        deletePost(id: ID!):Post!
-        createComment(data: CreateCommentInput!): Comment!
-        deleteComment(id: ID!):Comment!
-    }
-
-    input CreateUserInput {
-        name: String!
-        email: String!
-        age: Int
-    }
-
-    input CreatePostInput {
-        title: String! 
-        body: String! 
-        published: Boolean! 
-        authorId: ID!
-    }
-
-    input CreateCommentInput {
-        text: String!
-        authorId: ID!
-        postId: ID!
-    }
-
-    type User {
-        id: ID!
-        name: String!
-        email: String!
-        age: Int
-        posts: [Post!]!
-        comments: [Comment!]!
-    }
-
-    type Post {
-        id: ID!
-        title: String!
-        body: String!
-        published: Boolean!
-        author: User!
-        comments: [Comment!]!
-    }
-
-    type Comment {
-        id: ID!
-        text: String!
-        author: User!
-        post: Post!
-    }
-`
-
 // Resolvers ..   
 const resolvers = {
     Query: {
@@ -281,7 +217,7 @@ const resolvers = {
 }
 
 const server = new GraphQLServer({
-    typeDefs,
+    typeDefs: './src/schema.graphql',
     resolvers
 })
 
